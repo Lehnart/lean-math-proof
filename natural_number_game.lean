b3a233fb943024156265e0945ac6b8a824d7e048
Lehnart <thomasserre63@gmail.com>
2022-01-07 13:02:38 +0100

    modification on nat exos

--- natural_number_game.lean
+++ natural_number_game.lean
@@ -1,28 +1,17 @@
+example (x y : nat) (h : y = x + 7) : 2 * y = 2 * (x + 7) :=
+  calc 
+     2 * y = 2 * (x + 7) : congr_arg (nat.mul 2) h
 
-inductive mynat : Type
-| zero : mynat
-| succ : mynat → mynat
+example (a b : nat) (h : nat.succ a = b) : nat.succ( nat.succ(a)) = nat.succ(b) :=
+  calc 
+    nat.succ( nat.succ(a)) = nat.succ(b)  : congr_arg nat.succ h
 
-#check @mynat.rec_on
+example (a : nat) : a + nat.succ(0) = nat.succ(a) :=
+  calc 
+    a + nat.succ(0) = nat.succ(a) : rfl
 
-namespace mynat
-
-def add (m n : mynat) : mynat :=
-  mynat.rec_on 
-    n 
-    m 
-    (fun (n add_m_n : mynat), mynat.succ add_m_n)
-
-#reduce add (succ zero) (succ (succ zero))
-
-instance : has_zero mynat := has_zero.mk zero
-instance : has_add mynat := has_add.mk add
-
-theorem add_zero (m : mynat) : m + 0 = m := 
-  rfl
-
-theorem add_succ (m n : mynat) : m + succ n = succ (m + n) := 
-  rfl
-
-end mynat
-
+example (a : nat) : 0 + a = a :=
+  calc 
+     0 + a = 0 + a : rfl
+     ...   = a + 0 : nat.add_comm 0 a
+     ...   = a : rfl
