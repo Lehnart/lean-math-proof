413469d29c042add1abc1bd53dfb2445139d8d7f
Lehnart <thomasserre63@gmail.com>
2022-01-12 12:32:14 +0100

    Condition suffisante pour qu'un singleton soit sous ensemble d'un ensemble

--- math.lean
+++ math.lean
@@ -24,13 +24,49 @@ theorem set_ext_rec (E F : set α) ( x : α ) :  E = F → (∀x, ( E x ↔ F x 
 def empty_set : (set α) :=
   (fun (a:α), false)
 
-/- On peut toujours construire un singleton -/
-def singleton_set : (α -> set α) :=
+/- On peut toujours construire un singleton à partir d'une variable -/
+def singl {α}: (α -> set α) :=
   (fun (a:α), 
     (fun (b:α),
       a=b
     )
   )
 
-#check 
-example 
+example (a : α) : singl a a :=
+  rfl
+
+/- L'appartenance au singleton se réduit à l'égalité à l'élément du signleton -/
+example (a x : α): singl a x -> a = x :=
+  (fun h : singl a x,
+    h
+  )
+
+/- On peut toujours construire un ensemble à partir d'une liste -/
+def list_set {α}: (list α -> set α)
+| list.nil        := empty_set α 
+| (list.cons a l) := (fun x:α, (a=x) ∨ (list_set l x) )
+
+/- 
+  Definition de la notion de sous ensemble 
+  s1 est un sous ensemble de s2 
+-/
+def subset {α} (s₁ s₂ : set α) :=
+∀ ⦃a⦄, a ∈ s₁ → a ∈ s₂
+
+example (E F : set α) : subset E F <-> ∀ ⦃a⦄, a ∈ E → a ∈ F :=
+  iff.intro
+    (fun h : subset E F, h)
+    (fun h : ∀ ⦃a⦄, a ∈ E → a ∈ F, h)
+
+
+/- Une condition suffisante pour qu'un singleton soit un sous ensemble de E est que l'élément appartienne à E -/
+example (a : α) (E : set α) : E a -> (subset (singl a) E) :=
+(fun h : E a,
+  (fun x : α,
+    (fun h_a_x : a = x ,
+      eq.rec h h_a_x
+    ) 
+  )
+)
+
+
