d75fb1c1eab6dc0dbf0a0d5cb207ff2cf09ea7b6
Lehnart <thomasserre63@gmail.com>
2022-01-27 19:04:45 +0100

    Correct map restriction

--- math.lean
+++ math.lean
@@ -617,7 +617,7 @@ def pair_set (a b : set α) : set (pair α) :=
   )
 
 /- Une application c'est une fonction, un ensemble de départ et un ensemble de fin -/
-structure map  :=
+structure map (α : Type u) (β : Type u)  :=
   mk :: (A : set α) (B : set β) (f : α → β)
 
 /- Deux fonctions sont égales si chaque image est égale-/
@@ -633,5 +633,5 @@ def map_const (A : set α) (a : α) := map.mk A A (fun x:α, a)
 def map_canonic (A1 : set α) (A2 : set α) (h : subset A1 A2) := map.mk A1 A2 (fun x:α, x)
 
 /- Restriction d'une application -/
-def map_restriction (A1 : set α) (A2 : set α) (B : set β) (h : subset A1 A2) (f : α -> β) (m : map (set α) (set α)):= map.mk A1 B f
+def map_restriction (A : set α) (m : map α β) (h : subset A m.A) := map.mk A m.B m.f
 
